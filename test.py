552470375e54c0211bcee95762db1903b06b1068
admin <admin@adminwwdeMacBook-Pro.local>
2024-12-19 13:55:41 +0800

    del key info

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import requests
 url='https://test.tanka.ai/getusername'
 url2='https://test.shanda.com/getusername'
-testkey='AKIAYRMA5KYVCKOUTGPZ'
+response=requests.get(url)
+print(response.text)
